5280b240739e13f85e3d9f943228790d85ee0d92
saraec <sarachristensen1701@gmail.com>
2022-04-13 12:14:01 -0500

    array format

--- js/project.js
+++ js/project.js
@@ -26,6 +26,42 @@
 //    $("#myCarousel").carousel("next");
 //  });
 //});
+
+// array for images
+// var imgArray = new Array();
+//
+// imgArray[0] = new Image();
+// imgArray[0].src = 'images/img/Splash_image1.jpg';
+//
+// imgArray[1] = new Image();
+// imgArray[1].src = 'images/img/Splash_image2.jpg';
+//
+// /* ... more images ... */
+//
+// imgArray[5] = new Image();
+// imgArray[5].src = 'images/img/Splash_image6.jpg';
+//
+// /*------------------------------------*/
+//
+// function nextImage(element)
+// {
+//     var img = document.getElementById(element);
+//
+//     for(var i = 0; i < imgArray.length;i++)
+//     {
+//         if(imgArray[i].src == img.src) // << check this
+//         {
+//             if(i === imgArray.length){
+//                 document.getElementById(element).src = imgArray[0].src;
+//                 break;
+//             }
+//             document.getElementById(element).src = imgArray[i+1].src;
+//             break;
+//         }
+//     }
+// }
+
+
 var mod = 0;
 
 function sortStar() {
