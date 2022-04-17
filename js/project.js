22cca8fc6faca090b701d2119a011611ce1d8a57
rdiaz28695 <98487309+rdiaz28695@users.noreply.github.com>
2022-04-16 22:45:47 -0700

    fixed the webpage
    Worked with brother and fixed website, now pictures come up

--- js/project.js
+++ js/project.js
@@ -4,68 +4,68 @@
  **/
 
  function birthDate(){
-   var image = document.getElementById("star");
    var month=document.getElementById("month").value;
    var day=document.getElementById("day").value;
    var starArray=["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"];
+   var imagesArray=["img/aries.png", "img/Taurus.png", "img/gemini.png", "img/cancer.png", "img/Leo.png", "img/virgo.png", "img/libra.png","img/scorpio.png","img/Sagittarius.png","img/Capricorn.png","img/aquarius.png","img/pisces.png"];
    var url;
    var sign=[];
+   var image =[];
    if (month===""||day===""){
      alert("Enter the month and date to get your results!");
    }
    else{
-   if( (month ==3 && 20<day < 22)||(month==4 && day<20) ){
+   if( (month ==3 && day >= 21)||(month==4 && day<=19) ){
    sign= starArray[0];
-     url="../img/aries.png";
+   image=imagesArray[0];
    }
-   else if ( (month ==4 && 19 < day )||(month==5 && day<20) ){
+   else if ( (month ==4 && day >= 20 )||(month==5 && day<=20) ){
    sign= starArray[1];
-     url="../img/taurus.png";
+   image=imagesArray[1];
    }
-   else if ( (month ==5 && 20 < day )||(month==6 && day<21) ){
+   else if ( (month ==5 && day >= 21 )||(month==6 && day<=20) ){
    sign= starArray[2];
-     url="../img/gemini.png";
+     image=imagesArray[2];
    }
-   else if ( (month ==6 && 20 < day )||(month==7 && day<23) ){
+   else if ( (month ==6 && day >= 21 )||(month==7 && day<=22) ){
    sign= starArray[3];
-     url="../img/cancer.png";
+     image=imagesArray[3];
    }
-   else if ( (month ==7 && 22 < day )||(month==8 && day<23) ){
+   else if ( (month ==7 && day >= 23 )||(month==8 && day<=22) ){
    sign= starArray[4];
-     url="../img/Leo.png";
+     image=imagesArray[4];
    }
-   else if ( (month ==8 && 22 < day )||(month==9 && day<23) ){
+   else if ( (month ==8 && day >= 23 )||(month==9 && day<=22) ){
    sign= starArray[5];
-     url="../img/virgo.png";
+     image=imagesArray[5];
    }
-   else if ( (month ==9 && 22 < day )||(month==10 && day<23) ){
+   else if ( (month ==9 && day >= 23 )||(month==10 && day<=22) ){
    sign= starArray[6];
-     url="../img/libra.png";
+     image= imagesArray[6];
    }
-   else if ( (month ==10 && 22 < day )||(month==11 && day<22) ){
+   else if ( (month ==10 && day >= 23 )||(month==11 && day<=21) ){
    sign= starArray[7];
-     url="../img/scorpio.png";
+     image=imagesArray[7];
    }
-   else if ( (month ==11 && 21 < day )||(month==12 && day<22) ){
+   else if ( (month ==11 && day >= 22 )||(month==12 && day<=21) ){
    sign= starArray[8];
-     url="../img/Sagittarius.png";
+     image=imagesArray[8];
    }
-   else if ( (month ==12 && 21 < day )||(month==1 && day<20) ){
+   else if ( (month ==12 && day >= 22 )||(month==1 && day<=19) ){
    sign= starArray[9];
-     url="../img/Capricorn.png";
+     image=imagesArray[9];
    }
-   else if ( (month ==1 && 19 < day )||(month==2 && day<19) ){
+   else if ( (month ==1 && day >= 20 )||(month==2 && day<=18) ){
    sign= starArray[10];
-     url="../img/aquarius.png";
+     image=imagesArray[10];
    }
-   else if ( (month ==2 && 18 < day )||(month==3 && day<21) ){
+   else if ( (month ==2 && day >= 19 )||(month==3 && day<=20) ){
    sign= starArray[11];
-     url="../img/pisces.png";
+     image=imagesArray[11];
    }
 
    document.getElementById("display").innerHTML= sign;
-   image = url;
-   document.getElementById("star").src = image;
+   document.getElementById("zodiac").src= image;
  }
  }
 
