4be12d4933aa1e9793fb04662ad227d97d237686
saraec <sarachristensen1701@gmail.com>
2022-04-17 21:58:47 -0500

    attribute

--- js/project.js
+++ js/project.js
@@ -1,6 +1,7 @@
 /**
- * Author:    Sara Christensen, Arturo Rios, Rodrigo Diaz
- * Created:   4.15.2022
+* Cecilia (https://codepen.io/ceciliayang/pen/xVEjvP)
+ * Sara Christensen, Arturo Rios, Rodrigo Diaz, Henry Wilkinson
+ *  4.13.2022
  **/
 
  function birthDate(){
